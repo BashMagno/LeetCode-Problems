230289541580666758ae49b3f9a3992df984ffe3
M4GNO <115299343+BashMagno@users.noreply.github.com>
2023-10-24 18:15:55 +0200

    Create 9.Palondrime-number.java

---
+++ 9.Palondrime-number.java
@@ -0,0 +1,53 @@
+/*
+Given an integer x, return true if x is a 
+palindrome
+, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+ 
+
+Follow up: Could you solve it without converting the integer to a string?
+*/
+//RUNTIME
+/*
+Runtime
+      Details
+      6ms
+      Beats 39.98%of users with Java
+Memory
+      Details
+      42.30MB
+      Beats 96.65%of users with Java
+*/
+class Solution {
+    public static boolean isPalindrome(int x) {
+        String num = Integer.toString(x);
+        for (int i = 0; i < num.length() / 2; i++) {
+            if (num.charAt(i) != num.charAt(num.length() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
