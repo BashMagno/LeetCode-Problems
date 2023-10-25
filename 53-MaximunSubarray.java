7319f5576f914f0067e9053625cfa2d4808acec8
M4GNO <115299343+BashMagno@users.noreply.github.com>
2023-10-25 17:07:10 +0200

    Create 53-MaximunSubarray.java

---
+++ 53-MaximunSubarray.java
@@ -0,0 +1,54 @@
+/*
+Given an integer array nums, find the 
+subarray
+ with the largest sum, and return its sum.
+
+ 
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Explanation: The subarray [1] has the largest sum 1.
+Example 3:
+
+Input: nums = [5,4,-1,7,8]
+Output: 23
+Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+*/
+//RUNTIME
+/*
+Runtime
+      Details
+      1ms
+      Beats 100.00%of users with Java
+Memory
+      Details
+      59.23MB
+      Beats 57.32%of users with Java
+*/
+//Kadane's algorithm
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        
+        return maxSum;
+    }
+}
