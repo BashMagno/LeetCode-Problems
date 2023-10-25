6ba2f5ff4be41b7bf61642baa14e67e2dd737e89
M4GNO <115299343+BashMagno@users.noreply.github.com>
2023-10-25 16:53:37 +0200

    Update 14-LongestCommonPrefix.java

--- 14-LongestCommonPrefix.java
+++ 14-LongestCommonPrefix.java
@@ -1,1 +1,62 @@
-/
+/*
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters.
+*/
+//RUNTIME
+/*
+Runtime
+      Details
+      1ms
+      Beats 82.75%of users with Java
+Memory
+      Details
+      39.98MB
+      Beats 89.96%of users with Java
+*/
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        
+        int minLength = Integer.MAX_VALUE;
+        for (String str : strs) {
+            minLength = Math.min(minLength, str.length());
+        }
+        
+        StringBuilder prefix = new StringBuilder();
+        
+        for (int i = 0; i < minLength; i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (strs[j].charAt(i) != c) {
+                    return prefix.toString();
+                }
+            }
+            prefix.append(c);
+        }
+        
+        return prefix.toString();
+    }
+}
+
